e2d26a382757a1ccc04eee3065b1a770ae8a7a2a
Shawn O'Hare <ohare.shawn@gmail.com>
2013-09-22 20:53:55 -0700

    removed reference to KFold

--- kNN.py
+++ kNN.py
@@ -3,21 +3,19 @@ from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import KFold, cross_val_score
 
-
-#KF = KFold(n=150, n_folds=num_fold, shuffle=True)
-
 def main():
     iris = datasets.load_iris()
     k=input("Number of neighbors:")
     n=input("Number of folds:")
-    print(oss_error(k,n,iris.data, iris.target))
+    print(oss_error(k, n, iris.data, iris.target))
     
-
 def oss_error(k,n, X,Y):
     """ Does n-fold cross validation using knn and returns the mean score.
         Inputs: 
             k (int) - the number of nearest neighbors used to vote
             n (int) - the number of folds to use
+            X (array) - an array of feature vectors
+            Y (array) - an array of labels for X
         Returns:
             The mean of the knn classifier scores for held-out validation sets.
     """
@@ -27,4 +25,3 @@ def oss_error(k,n, X,Y):
 
 if __name__ == "__main__":
     main()
-  
